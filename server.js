93735ea4db42e9eec66f88fe28c80413839e404b
ozlevka-ericom <lev.ozeryansky@ericom.com>
2019-05-30 16:59:21 +0300

    Adapt for simple upload files

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
-const express = require('express')
-const bodyParser= require('body-parser')
-const app = express()
+const express = require('express');
+const bodyParser= require('body-parser');
+const app = express();
 const multer = require('multer');
-fs = require('fs-extra')
-app.use(bodyParser.urlencoded({extended: true}))
+fs = require('fs-extra');
+app.use(bodyParser.urlencoded({extended: true}));
 
-const MongoClient = require('mongodb').MongoClient
-ObjectId = require('mongodb').ObjectId
 
-const myurl = 'mongodb://localhost:27017';
 
 
 var storage = multer.diskStorage({
@@ -18,17 +15,20 @@ var storage = multer.diskStorage({
   filename: function (req, file, cb) {
     cb(null, file.fieldname + '-' + Date.now())
   }
-})
+});
 
-var upload = multer({ storage: storage })
+var upload = multer({
+    storage: storage,
+    fileFilter: function(req, file, cb) {
+      if(file.mimetype === "application/json") {
+        cb(null,true);
+      } else {
+        cb(new Error(`file type: ${file.mimetype} not allowed`));
+      }
 
-MongoClient.connect(myurl, (err, client) => {
-  if (err) return console.log(err)
-  db = client.db('test') 
-  app.listen(3000, () => {
-    console.log('listening on 3000')
-  })
-})
+  }
+});
+
 
 app.get('/',function(req,res){
   res.sendFile(__dirname + '/index.html');
@@ -38,80 +38,30 @@ app.get('/',function(req,res){
 // upload single file
 
 app.post('/uploadfile', upload.single('myFile'), (req, res, next) => {
-  const file = req.file
+  const file = req.file;
   if (!file) {
-    const error = new Error('Please upload a file')
-    error.httpStatusCode = 400
-    return next(error)
+    const error = new Error('Please upload a file');
+    error.httpStatusCode = 400;
+    return next(error);
 
   }
-
- 
     res.send(file)
  
-})
+});
 //Uploading multiple files
 app.post('/uploadmultiple', upload.array('myFiles', 12), (req, res, next) => {
-  const files = req.files
+  const files = req.files;
   if (!files) {
-    const error = new Error('Please choose files')
-    error.httpStatusCode = 400
+    const error = new Error('Please choose files');
+    error.httpStatusCode = 400;
     return next(error)
   }
 
-    res.send(files)
+  res.send(files);
  
-})
-
-
-app.post('/uploadphoto', upload.single('picture'), (req, res) => {
-	var img = fs.readFileSync(req.file.path);
- var encode_image = img.toString('base64');
- // Define a JSONobject for the image attributes for saving to database
- 
- var finalImg = {
-      contentType: req.file.mimetype,
-      image:  new Buffer(encode_image, 'base64')
-   };
-db.collection('mycollection').insertOne(finalImg, (err, result) => {
-  	console.log(result)
-
-    if (err) return console.log(err)
-
-    console.log('saved to database')
-    res.redirect('/')
-  
-    
-  })
-})
-
-
-app.get('/photos', (req, res) => {
-db.collection('mycollection').find().toArray((err, result) => {
-
-  	const imgArray= result.map(element => element._id);
-			console.log(imgArray);
-
-   if (err) return console.log(err)
-   res.send(imgArray)
-
-  
-   
-  })
 });
 
-app.get('/photo/:id', (req, res) => {
-var filename = req.params.id;
-
-db.collection('mycollection').findOne({'_id': ObjectId(filename) }, (err, result) => {
-
-    if (err) return console.log(err)
-
-   res.contentType('image/jpeg');
-   res.send(result.image.buffer)
-  
-   
-  })
-})
-
+app.listen(8080, () => {
+  console.log(`I'm listening`)
+});
 
